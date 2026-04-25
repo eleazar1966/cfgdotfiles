eff99010d039fb85656d777475f9c0d2606b3ba2
eleazar1966 <anzola@gmail.com>
2026-04-25 11:21:07 -0400

    Auto-update configurations @ 2026-04-25 11:21:07

--- .local/share/systools/launch-rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Rofi lee el archivo generado por Matugen cada vez que se ejecuta.
-rofi -show drun -show-icons -theme ~/.config/rofi/colors.rasi
--- .local/share/systools/pgadmin4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Activar el entorno virtual e iniciar pgAdmin 4
-source ~/pgadmin4/venv/bin/activate
-python ~/pgadmin4/venv/lib/python3.13/site-packages/pgadmin4/pgAdmin4.py
